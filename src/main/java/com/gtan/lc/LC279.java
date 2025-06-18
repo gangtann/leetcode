94234148c5d18a7741f3009777a648f6ffcfb353
Gang Tan <gangtan@foxmail.com>
2025-06-18 14:00:56 +0800

    更新 LC279 完全平方数 代码注释

--- src/main/java/com/gtan/lc/LC279.java
+++ src/main/java/com/gtan/lc/LC279.java
@@ -14,20 +14,27 @@ public class LC279 {
 
     class Solution {
         public int numSquares(int n) {
+            // 首先找到小于n的完全平方数
             List<Integer> squares = new ArrayList<>();
             for (int i = 1; i <= Math.sqrt(n); i++) {
                 squares.add(i * i);
             }
+            // dp[sum]: 和为 sum 的完全平方数的最少数量
             int[] dp = new int[n + 1];
+            // 初始化为 n + 1, 因为后面要比较最少数量
             Arrays.fill(dp, n + 1);
+            // 初始化: 和为 0 的完全平方数的最少数量为 0
             dp[0] = 0;
-            for (int i = 1; i <= n; i++) {
+            // 自底向上遍历每一个和(递推 + 记忆化搜索)
+            for (int sum = 1; sum <= n; sum++) {
+                // 从不同路径中选择数量最少的路径
                 for (int square : squares) {
-                    if (i - square >= 0) {
-                        dp[i] = Math.min(dp[i], dp[i - square] + 1);
+                    if (sum - square >= 0) {
+                        dp[sum] = Math.min(dp[sum], dp[sum - square] + 1);
                     }
                 }
             }
+            // 返回和为 sum = n 的完全平方数的最少数量
             return dp[n];
         }
     }

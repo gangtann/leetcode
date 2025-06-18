b1ca5726cd6f8fde0d5afdd636ed55bc44f15149
Gang Tan <gangtan@foxmail.com>
2025-06-18 11:43:54 +0800

    添加 LC70 爬楼梯 新的代码实现

--- src/main/java/com/gtan/lc/LC70.java
+++ src/main/java/com/gtan/lc/LC70.java
@@ -23,4 +23,17 @@ public class LC70 {
         }
     }
 
+    class Solution2 {
+        public int climbStairs(int n) {
+            int pre2 = 1, pre1 = 1;
+            int cur = 1;
+            for (int i = 2; i <= n; i++) {
+                cur = pre1 + pre2;
+                pre2 = pre1;
+                pre1 = cur;
+            }
+            return cur;
+        }
+    }
+
 }

659087f027593790271e7d36167b52ef47f020c4
Gang Tan <gangtan@foxmail.com>
2025-06-18 14:27:07 +0800

    更新 LC322 零钱兑换 代码注释

--- src/main/java/com/gtan/lc/LC322.java
+++ src/main/java/com/gtan/lc/LC322.java
@@ -12,16 +12,22 @@ public class LC322 {
 
     class Solution {
         public int coinChange(int[] coins, int amount) {
+            // dp[sum]: 可以凑成总金额为 sum 所需的最少的硬币个数
             int[] dp = new int[amount + 1];
+            // 因为之后要比较最少的硬币个数, 初始化为一个不可能的最大值
             Arrays.fill(dp, amount + 1);
+            // 可以凑成总金额为 0 所需的最少的硬币个数为 0
             dp[0] = 0;
-            for (int i = 1; i <= amount; i++) {
+            // 动态规划: 自底向上(递推 + 记忆化搜索)
+            for (int sum = 1; sum <= amount; sum++) {
+                // 从不同的路径中选择硬币最少的路径
                 for (int coin : coins) {
-                    if (coin <= i) {
-                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    if (coin <= sum) {
+                        dp[sum] = Math.min(dp[sum], dp[sum - coin] + 1);
                     }
                 }
             }
+            // 返回可以凑成总金额为 sum = n 所需的最少的硬币个数; 如果不存在则返回 -1
             if (dp[amount] == amount + 1) {
                 return -1;
             } else {

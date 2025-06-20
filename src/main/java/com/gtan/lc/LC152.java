2b98f70a2f26c9afb037f3a3d3086cc96b6ae8b2
Gang Tan <gangtan@foxmail.com>
2025-06-20 10:12:43 +0800

    更新 LC152 乘积最大子数组 完善注释

--- src/main/java/com/gtan/lc/LC152.java
+++ src/main/java/com/gtan/lc/LC152.java
@@ -9,33 +9,29 @@ package com.gtan.lc;
 public class LC152 {
 
     /**
-     * 思路和 <300. 最长递增子序列> 类似, 但有一些不同的地方需要区分
-     * 1. 题目要求的是连续子数组, 因此不需要和前面每一个元素比较, 只需要比较前一个元素就行
-     * 2. 因此我们不需要使用dp数组, 用一个变量记录即可
-     * 3. 因为涉及到正数与负数间的乘法, 结果随时有可能从最大值变为最小值, 也可能从最小值变为最大值, 因此需要记录最小值与最大值
-     * dp_min: 以第 i 个元素为结尾的非空连续子数组, 记录下其乘积的最小值
-     *      初始值: nums[0], 因为数组至少有一个元素
-     *      递推公式: dp_min = max(dp_max * nums[i], dp_min * nums[i]), nums[i])
-     *      nums[i]需要参与比较的原因是它可能是最小值也可能是最大值:
-     *          例如 [-1, -2], 以 -2 结尾的元素最小值就是 -2
-     *          例如 [-1, 2], 以 2 结尾的元素最大值就是 2
-     *          因此当前元素可能是最小值也可能是最大值
-     * dp_max: 以第 i 个元素为结尾的非空连续子数组, 记录下其乘积的最大值
-     *      初始值: nums[0], 因为数组至少有一个元素
-     *      递推公式: dp_min = min(dp_max * nums[i], dp_min * nums[i]), nums[i])
-     * ans: 从每一个以 i 结尾的最大乘积中选择最大值就是最终的结果
+     * 本题只有前一个元素会影响当前状态
      */
     class Solution {
         public int maxProduct(int[] nums) {
-            // ans 直接初始化为nums[0], 因为数组至少有一个元素
+            /*
+             * dp数组的定义是: 以第 i 个元素为结尾的乘积最大子数组的最大乘积
+             * 只不过第 i 个元素只会被前一个元素影响(以第 i-1 个元素为结尾的子数组), 因此只需要用一个变量保存
+             * 又因为本题的特殊性, 第 i 个元素可能是正数也可能是负数,
+             * 如果是负数, 以第 i-1 个元素为结尾的乘积最小子数组的最小乘积, 与第 i 个元素相乘, 也可能是最大值
+             * 因此还需要记录以第 i 个元素为结尾的乘积最小子数组的最小乘积
+             * 初始化: 至少有一个元素, 初始化为nums[0]
+             * */
+            int dpMax = nums[0];
+            int dpMin = nums[0];
+            // dp数组中的最大值就是乘积最大子数组的最大乘积
             int ans = nums[0];
-            int dp_min = nums[0], dp_max = nums[0];
             for (int i = 1; i < nums.length; i++) {
-                // 必须用 temp 记录 dp_max, 否则计算 dp_min 时 dp_max 已经更新
-                int temp = dp_max;
-                dp_max = Math.max(Math.max(dp_max * nums[i], dp_min * nums[i]), nums[i]);
-                dp_min = Math.min(Math.min(temp * nums[i], dp_min * nums[i]), nums[i]);
-                ans = Math.max(dp_max, ans);
+                int tempMax = nums[i] * dpMax;
+                int tempMin = nums[i] * dpMin;
+                // nums[i]可能是最小值也可能是最大值
+                dpMax = Math.max(Math.max(tempMax, tempMin), nums[i]);
+                dpMin = Math.min(Math.min(tempMax, tempMin), nums[i]);
+                ans = Math.max(ans, dpMax);
             }
             return ans;
         }
